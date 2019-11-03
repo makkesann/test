d5cb0a6568cbbc1806b924b38d595465357d06b5
makkesann <makkesann@gmail.com>
2019-11-03 14:20:29 +0900

    initialize

---
+++ db/migrate/20191030073208_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.references :member, null: false
+      t.string :title
+      t.string :isbn
+      t.string :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
