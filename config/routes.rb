95b1c43e3e0e8eb00ec87bc14bc074184cd22f01
guan <kuniyasu.guan@gmail.com>
2019-11-06 17:36:14 +0900

    nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do 
+    resources :books
+  end
 end
